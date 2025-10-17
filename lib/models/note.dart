9ff5b4fc301ce8a1d97c8ba708b8838829878202
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-17 18:41:36 +0300

    note model

---
+++ lib/models/note.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:equatable/equatable.dart';
+
+class Note extends Equatable {
+  final String id;
+  final String title;
+  final String body;
+
+  const Note({
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  factory Note.fromJson(String source) =>
+      Note.fromMap(json.decode(source));
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      body: map['body'],
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'title': title,
+    'body': body,
+  };
+
+  @override
+  List<Object?> get props => [id, title, body];
+}
